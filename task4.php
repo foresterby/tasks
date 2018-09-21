192e506334053ba17b44e7b0eb97e394d2573e44
foresterby <38014857+foresterby@users.noreply.github.com>
2018-09-21 12:04:37 +0300

    Update task4.php

--- task4.php
+++ task4.php
@@ -1,4 +1,3 @@
-
 <?php
 $random_arr = [];
 for ($i = 1; $i <= 10; $i++)
@@ -6,31 +5,26 @@ for ($i = 1; $i <= 10; $i++)
 
 
 
-$myArrayObject = new ArrayObject($random_arr);
 
+echo '<br>'.'Начальный массив:'.'<br>';
 print_r($random_arr);
 $max_value = max($random_arr);
 $min_value = min($random_arr);
-echo '<br>'."$max_value";
-echo '<br>'."$min_value".'<br>';
+echo '<br><br>'.'Максимальное значение: '."$max_value";
+echo '<br>'.'Минимальное значение: '."$min_value".'<br>';
 
 
 
 $key_max_value = array_search( (max($random_arr)), $random_arr);
 $key_min_value = array_search( (min($random_arr)), $random_arr);
-echo '<br>'."$key_max_value";
-echo '<br>'."$key_min_value".'<br>';
+echo '<br>'.'Ключ максимального значения: '."$key_max_value";
+echo '<br>'.'Ключ минимального значения: '."$key_min_value".'<br>';
 
 
 $random_arr[$key_max_value] = "$min_value";
 $random_arr[$key_min_value] = "$max_value";
-
+echo '<br>'.'Итоговый массив:'.'<br>';
 print_r($random_arr);
-
-
-
-
-
 
 ?>	
 
