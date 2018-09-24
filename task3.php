00bb51e2887c088f0b3e99e3cc4d3731a5773b26
foresterby <38014857+foresterby@users.noreply.github.com>
2018-09-24 09:26:41 +0300

    Update task3.php

--- task3.php
+++ task3.php
@@ -1,19 +1,8 @@
-
 <?php
-$arr = [];
+$result = 0;
 for ($i = 20; $i <= 45; $i++)
-	$arr[] = $i;
-	
-$arr_result = [];	
-foreach ($arr as $i) {
 	if (fmod($i, '5') == 0)
-		$arr_result[] = $i;
-	}
+		$result = $result + $i;
 	
-$result = array_sum($arr_result);
 echo $result;
-
-?>	
-
-	
-	
+?>
