faae010998d0f0902753d38fb65973e4c2150f14
foresterby <38014857+foresterby@users.noreply.github.com>
2018-09-25 17:36:34 +0300

    Update task4.php

--- task4.php
+++ task4.php
@@ -1,32 +1,46 @@
 <?php
 $random_arr = [];
 for ($i = 1; $i <= 10; $i++)
-	$random_arr[]=rand(50, 100);
-
+	$random_arr[] = rand(50, 100);
 
 
 
 echo '<br>'.'Начальный массив:'.'<br>';
 print_r($random_arr);
-$max_value = max($random_arr);
-$min_value = min($random_arr);
-echo '<br><br>'.'Максимальное значение: '."$max_value";
-echo '<br>'.'Минимальное значение: '."$min_value".'<br>';
+
+		
+$max_value = $random_arr[0];
+$min_value = $random_arr[0];
 
 
+for($i = 0; $i < count($random_arr); $i++) 
+	{
+	
+	if($random_arr[$i] > $max_value)
+		{
+		$max_value = $random_arr[$i];
+		$key_max = $i;
+		}
+	
+	
+	if($random_arr[$i] < $min_value)
+		{
+		$min_value = $random_arr[$i];
+		$key_min = $i;
+		}		
+	}
+	
 
-$key_max_value = array_search( (max($random_arr)), $random_arr);
-$key_min_value = array_search( (min($random_arr)), $random_arr);
-echo '<br>'.'Ключ максимального значения: '."$key_max_value";
-echo '<br>'.'Ключ минимального значения: '."$key_min_value".'<br>';
+//echo '<br>'."$key_max";
+//echo '<br>'."$key_min";
 
-
-$random_arr[$key_max_value] = "$min_value";
-$random_arr[$key_min_value] = "$max_value";
-echo '<br>'.'Итоговый массив:'.'<br>';
+	
+$random_arr[(int)$key_max] = "$min_value";
+$random_arr[(int)$key_min] = "$max_value";
+	
+echo '<br>';
+echo '<br>'.'Массив после преобразования:'.'<br>';
 print_r($random_arr);
 
-?>	
 
-	
-	
+?>
