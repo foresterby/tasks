e9bd1534406add24ec0ccf1bc48ca2d606893592
foresterby <38014857+foresterby@users.noreply.github.com>
2018-09-26 12:05:59 +0300

    Update task4.php

--- task4.php
+++ task4.php
@@ -3,8 +3,6 @@ $random_arr = [];
 for ($i = 1; $i <= 10; $i++)
 	$random_arr[] = rand(50, 100);
 
-
-
 echo '<br>'.'Начальный массив:'.'<br>';
 print_r($random_arr);
 
@@ -27,20 +25,14 @@ for($i = 0; $i < count($random_arr); $i++)
 		{
 		$min_value = $random_arr[$i];
 		$key_min = $i;
-		}		
+		}
 	}
 	
+//echo '<br>'."$max_value";
+$random_arr[(int)$key_min] = $max_value;
+$random_arr[(int)$key_max] = $min_value;
 
-//echo '<br>'."$key_max";
-//echo '<br>'."$key_min";
-
-	
-$random_arr[(int)$key_max] = "$min_value";
-$random_arr[(int)$key_min] = "$max_value";
-	
 echo '<br>';
-echo '<br>'.'Массив после преобразования:'.'<br>';
+echo '<br>'.'Измененный массив:'.'<br>';
 print_r($random_arr);
-
-
 ?>
